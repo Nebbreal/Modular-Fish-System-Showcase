cc03b89a1f34a606a7ea263c62c8359594ecb737
stewbyte <xgenyofficial@gmail.com>
2025-05-15 13:53:47 +0200

    Add min & max pressure values

--- Assets/Plugins/PISK/Scripts/PISKController.cs
+++ Assets/Plugins/PISK/Scripts/PISKController.cs
@@ -2,10 +2,27 @@ using UnityEngine;
 
 public class PISKController : MonoBehaviour
 {
-    static public float Pressure { get; private set; }
+    public static PISKController Instance { get; private set; }
+
+    [Header("Pressure")]
+    [SerializeField] public float Pressure { get; private set; }
+    [SerializeField] public float MinPressure { get; private set; } = 0f;
+    [SerializeField] public float MaxPressure { get; private set; } = 100f;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Debug.LogError("There can only be one PISKController per scene!");
+            return;
+        }
+
+        Instance = this;
+    }
 
     void Update()
     {
         Pressure += 0.1f;
     }
+
 }
