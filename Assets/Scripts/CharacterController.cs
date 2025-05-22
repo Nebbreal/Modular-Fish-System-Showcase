1bcbeaf55d1d45175715907fe0cb685a68312b87
WesleyBurgt <wesleyburgt@gmail.com>
2025-05-22 15:13:29 +0200

    Add fish generation

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,9 +5,11 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private float pressureToCatch = 95f;
     [SerializeField] private float pressureToReelIn = 50f;
     [SerializeField] PopUpController popUpScreen;
+    [SerializeField] private FishGenerator fishGenerator;
     private float pressure;
     private bool readyToReelIn = false;
-
+    GameObject generatedFish;
+    
     void Update()
     {
         UpdatePressure();
@@ -19,11 +21,26 @@ public class CharacterController : MonoBehaviour
         if (PISKController.Instance == null) return;
         pressure = PISKController.Instance.Pressure;
     }
+    
+    private void GenerateFish()
+    {
+        if(generatedFish) Destroy(generatedFish);
+    
+        GameObject fish = fishGenerator.GenerateFish();
+        fish.SetActive(true);
+        
+        generatedFish = fish;
+    
+        fish.transform.position = Vector3.zero;
+        fish.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+    }
 
     private void TryCatchFish()
     {
-        if (pressure > pressureToCatch)
+        if (pressure > pressureToCatch && !readyToReelIn)
         {
+            GenerateFish();
+            generatedFish.gameObject.transform.SetParent(popUpScreen.gameObject.transform);
             readyToReelIn = true;
         }
         else if (pressure < pressureToReelIn && readyToReelIn)
