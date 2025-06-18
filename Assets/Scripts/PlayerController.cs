e6a766da3653877212579fd28c17b6a199923cc0
WesleyBurgt <wesleyburgt@gmail.com>
2025-06-18 12:39:11 +0200

    Implement PilloSDK
    still uses PISK instead of Pillo if there is an instance

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Hulan.PilloSDK.DeviceManager;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -5,28 +6,42 @@ public class PlayerController : MonoBehaviour
     [SerializeField] PopUpController popUpScreen;
     [SerializeField] private FishingRodController fishingRodController;
 
-    private float pressure;
-    private float lastPressure;
-    private float maxPressure;
+    private float _pressure;
+    private float _lastPressure;
+    private float _maxPressure;
 
     void Start()
     {
-        if (PISKController.Instance == null) return;
-        maxPressure = PISKController.Instance.MaxPressure;
+        if (PISKController.Instance != null)
+        {
+            _maxPressure = PISKController.Instance.MaxPressure; 
+        }
+        else
+        {
+            PilloDeviceManager.onPeripheralPressureDidChange += HandlePressureChange;
+            _maxPressure = 1024f;
+        }
     }
 
     void Update()
     {
-        UpdatePressure();
-        fishingRodController.RotateWithPressure(pressure, lastPressure, maxPressure);
-        popUpScreen.HandleHidingPopUp(pressure);
-        fishingRodController.TryCatchFish(pressure);
+        if (PISKController.Instance != null)
+        {
+            _pressure = PISKController.Instance.Pressure;
+        }
+        
+        fishingRodController.RotateWithPressure(_pressure, _lastPressure, _maxPressure);
+        popUpScreen.HandleHidingPopUp(_pressure);
+        fishingRodController.TryCatchFish(_pressure);
     }
 
-    private void UpdatePressure()
+    private void LateUpdate()
     {
-        if (PISKController.Instance == null) return;
-        lastPressure = pressure;
-        pressure = PISKController.Instance.Pressure;
+        _lastPressure = _pressure;
+    }
+
+    private void HandlePressureChange(string identifier, int pressure)
+    {
+        _pressure = pressure / _maxPressure * 100f; //to convert to the pressure values (0-100)
     }
 }
